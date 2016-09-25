9e879441b240fba3b886281c36462246bab73d10
Charles Watson <sircharleswatson@gmail.com>
2016-09-24 20:13:49 -0500

    move index.html and index.js to root dir

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ require("font-awesome/css/font-awesome.css");
 // Require index.html so it gets copied to dist
 require("./index.html");
 
-var Elm = require("./elm/Main.elm");
+var Elm = require("./src/elm/Main.elm");
 var mountNode = document.getElementById("main");
 
 // The third value on embed are the initial values for incomming ports into Elm
